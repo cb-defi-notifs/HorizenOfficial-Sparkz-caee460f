29147d4839a458bee3c6c95c692e786ab4a4b28b
Paolo Galli <paologalli@horizenlabs.io>
2022-06-30 14:48:49 +0200

    SDK-143: refactor DnsClient class
    Signed-off-by: Paolo Galli <paologalli@horizenlabs.io>

--- src/main/scala/scorex/core/network/dns/DnsClient.scala
+++ src/main/scala/scorex/core/network/dns/DnsClient.scala
@@ -6,13 +6,13 @@ import scorex.core.network.dns.strategy.LookupStrategy
 import scorex.util.ScorexLogging
 
 class DnsClient(dnsClientParams: DnsClientInput) extends Actor with ScorexLogging {
-  import scorex.core.network.dns.DnsClient.ReceivableMessages.GetDnsSeeds
+  import scorex.core.network.dns.DnsClient.ReceivableMessages.LookupRequest
 
   override def receive: Receive =
     dnsLookup orElse nonsense
 
   private def dnsLookup: Receive = {
-    case GetDnsSeeds(strategy: LookupStrategy, dnsClientParams: DnsClientInput) => sender() ! strategy.apply(dnsClientParams)
+    case LookupRequest(strategy: LookupStrategy) => sender() ! strategy.apply(dnsClientParams)
   }
 
   private def nonsense: Receive = {
@@ -23,7 +23,7 @@ class DnsClient(dnsClientParams: DnsClientInput) extends Actor with ScorexLoggin
 
 object DnsClient {
   object ReceivableMessages {
-    case class GetDnsSeeds(s: LookupStrategy, o: DnsClientInput)
+    case class LookupRequest(s: LookupStrategy)
   }
 }
 
