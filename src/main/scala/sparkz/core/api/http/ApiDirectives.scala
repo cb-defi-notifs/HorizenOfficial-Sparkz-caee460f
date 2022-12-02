4d02022c852e09f50bb85ff22c15fa3fd89afb21
MarcoOl94 <marco.olivero94@libero.it>
2022-12-02 17:27:33 +0100

    Added Basic authentication directive

--- src/main/scala/sparkz/core/api/http/ApiDirectives.scala
+++ src/main/scala/sparkz/core/api/http/ApiDirectives.scala
@@ -1,5 +1,6 @@
 package sparkz.core.api.http
 
+import akka.http.scaladsl.server.directives.{AuthenticationDirective, Credentials}
 import akka.http.scaladsl.server.{AuthorizationFailedRejection, Directive0}
 import sparkz.core.settings.RESTApiSettings
 import sparkz.core.utils.SparkzEncoding
@@ -16,6 +17,24 @@ trait ApiDirectives extends CorsHandler with SparkzEncoding {
       val keyHashStr: String = encoder.encode(Blake2b256(key))
       if (settings.apiKeyHash.contains(keyHashStr)) pass
       else reject(AuthorizationFailedRejection)
+  }
+
+  lazy val withBasicAuth: AuthenticationDirective[String] = authenticateBasic(realm = "secure api", basicAuthentication)
+
+  def basicAuthentication(credentials: Credentials): Option[String] = {
+    settings.apiKeyHash match {
+      case None => None
+      case Some(key) =>
+        key.isEmpty match {
+          case true =>
+            None
+          case false =>
+            credentials match {
+              case p @ Credentials.Provided(id) if p.verify(key) => Some(id)
+              case _ => None
+            }
+        }
+    }
   }
 
 }
