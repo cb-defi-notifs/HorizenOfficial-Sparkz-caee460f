4c094b70f5473edc166cbe436418daa9484f308e
Andrea Carbone <acarbone@horizenlabs.io>
2024-05-31 18:05:35 +0200

    fixing if cycle in publish

--- ci/publish.sh
+++ ci/publish.sh
@@ -5,13 +5,13 @@ set -x
 
 retval=0
 
-if [[ "${DEV_RELEASE}" = "true" ]]; then
+if [[ "${DEV_RELEASE}" == "true" ]]; then
   echo "" && echo "=== Publishing development release on Sonatype Nexus repository. Timestamp is: $(date '+%a %b %d %H:%M:%S %Z %Y') ===" && echo ""
 #  sbt -ivy ./.ivy2 -sbt-dir ./.sbt +publish
-elif [[ "${RC_RELEASE}" = "true" ]]; then
+elif [[ "${RC_RELEASE}" == "true" ]]; then
   echo "" && echo "=== Publishing RC release on Maven repository. Timestamp is: $(date '+%Y-%m-%d %H:%M') ===" && echo ""
 #  sbt -ivy ./.ivy2 -sbt-dir ./.sbt +publishSigned sonatypeBundleRelease
-elif [[ "${PROD_RELEASE}" = "true" ]]; then
+elif [[ "${PROD_RELEASE}" == "true" ]]; then
   echo "" && echo "=== Publishing production release on Maven repository. Timestamp is: $(date '+%Y-%m-%d %H:%M') ===" && echo ""
 #  sbt -ivy ./.ivy2 -sbt-dir ./.sbt +publishSigned sonatypeBundleRelease
 else
