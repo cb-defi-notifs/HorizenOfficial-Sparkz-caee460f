62222a4de43a83c597a3ff9a84f94ffff155169b
Andrea Carbone <acarbone@horizenlabs.io>
2024-05-31 19:56:26 +0200

    Remove debug point

--- ci/publish.sh
+++ ci/publish.sh
@@ -5,19 +5,15 @@ set -x
 
 retval=0
 
-echo "" && echo "Debugging PROD RELEASE var in publish.sh: ${PROD_RELEASE}"
-echo "" && echo "Debugging RC RELEASE var in publish.sh: ${RC_RELEASE}"
-echo "" && echo "Debugging DEV RELEASE var in publish.sh: ${DEV_RELEASE}"
-
 if [[ "${DEV_RELEASE}" == "true" ]]; then
   echo "" && echo "=== Publishing development release on Sonatype Nexus repository. Timestamp is: $(date '+%a %b %d %H:%M:%S %Z %Y') ===" && echo ""
-#  sbt -ivy ./.ivy2 -sbt-dir ./.sbt +publish
+  sbt -ivy ./.ivy2 -sbt-dir ./.sbt +publish
 elif [[ "${RC_RELEASE}" == "true" ]]; then
   echo "" && echo "=== Publishing RC release on Maven repository. Timestamp is: $(date '+%Y-%m-%d %H:%M') ===" && echo ""
-#  sbt -ivy ./.ivy2 -sbt-dir ./.sbt +publishSigned sonatypeBundleRelease
+  sbt -ivy ./.ivy2 -sbt-dir ./.sbt +publishSigned sonatypeBundleRelease
 elif [[ "${PROD_RELEASE}" == "true" ]]; then
   echo "" && echo "=== Publishing production release on Maven repository. Timestamp is: $(date '+%Y-%m-%d %H:%M') ===" && echo ""
-#  sbt -ivy ./.ivy2 -sbt-dir ./.sbt +publishSigned sonatypeBundleRelease
+  sbt -ivy ./.ivy2 -sbt-dir ./.sbt +publishSigned sonatypeBundleRelease
 else
   echo "" && echo "=== Not going to publish!!! The build did not match either DEV, RC or PROD release requirements ===" && echo ""
 fi
